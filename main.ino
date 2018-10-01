126564f7e73141e080bd0ff6e433f309459016f8
makelk <42709957+makelk@users.noreply.github.com>
2018-10-01 13:47:05 +0100

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,19 @@
+int v = A0;
+float vout = 0.0;
+float  vin = 0.0;
+float r1 = 30000.0;
+float r2 = 7500.0;
+int val = 0;
+
+void setup() {
+  pinMode(v,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val  = analogRead(v);
+  vout = (val*5.0)/1024.0;
+  vin = vout/(r2/(r1+r2));
+  Serial.println(vin);
+delay(500);
+}
